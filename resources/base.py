1116aa128f09b866d5fb04ee7c8c11fb7b3f653b
iAliF <mr.ali.fotouhi@gmail.com>
2022-09-21 16:17:37 +0430

    Adding 'section_url' field to 'Base' class

--- resources/base.py
+++ resources/base.py
@@ -6,6 +6,9 @@ from .models import DataModel
 class Base:
     __base_url = 'https://gu.ac.ir/'
 
+    def __init__(self, section_url: str) -> None:
+        self._section_url = section_url
+
     def get_latest(self) -> DataModel:
         raise NotImplementedError
 
