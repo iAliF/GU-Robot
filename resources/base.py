4dcda5610c481c0f8902bd86c272ab38dd3a6ffd
iAliF <mr.ali.fotouhi@gmail.com>
2022-09-21 16:28:20 +0430

    Add logger to 'Base' class

--- resources/base.py
+++ resources/base.py
@@ -1,12 +1,20 @@
+import logging
 from typing import List
 
+from rich.logging import RichHandler
+
 from .models import DataModel
+
+logging.basicConfig(level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()])
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class Base:
     __base_url = 'https://gu.ac.ir/'
 
-    def __init__(self, section_url: str) -> None:
+    def __init__(self, name: str, section_url: str) -> None:
+        self._name = name
         self._section_url = section_url
 
     def get_latest(self) -> DataModel:
@@ -14,3 +22,7 @@ class Base:
 
     def get_all(self, page: int = 1) -> List[DataModel]:
         raise NotImplementedError
+
+    def log(self, level: int, message: str, *args, **kwargs) -> None:
+        message = f'{self._name} | {message}'
+        logger.log(level, message, *args, **kwargs)
