3c1b86c6c8b1bc2c478b29923cb0e563cb30b582
iAliF <mr.ali.fotouhi@gmail.com>
2022-09-21 16:16:00 +0430

    Base class

--- resources/base.py
+++ resources/base.py
@@ -1,2 +1,13 @@
+from typing import List
+
+from .models import DataModel
+
+
 class Base:
-    pass
+    __base_url = 'https://gu.ac.ir/'
+
+    def get_latest(self) -> DataModel:
+        raise NotImplementedError
+
+    def get_all(self, page: int = 1) -> List[DataModel]:
+        raise NotImplementedError
